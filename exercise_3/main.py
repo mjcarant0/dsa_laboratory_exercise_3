46c96f50639073af659c350777ceacd9cf12d22e
dnlaylo <laylomarga@gmail.com>
2025-10-19 20:28:07 +0800

    feat(main): add menu options list and print statements

--- exercise_3/main.py
+++ exercise_3/main.py
@@ -5,11 +5,15 @@ Main program to run the linked list menu.
 from menu import Menu
 
 def display_menu():  # Display menu options
-    pass
+    options = ["1. Insert Node", "2. Delete Node", "3. Search Node", "4. Display List", "5. Reverse List", "6. Exit"]
+
+    print("\n====== LINKED LIST MENU ======")
+    print("\n".join(options))
+    print("==============================\n")
 
 def main(): # Main function to run the menu loop
     """Create validation checker loop for menu choices and call appropriate methods."""
-    pass
+    display_menu()
 
 if __name__ == "__main__":
     main()
