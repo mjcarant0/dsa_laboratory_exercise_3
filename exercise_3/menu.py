29fd8f2ee09d1eeed9f1d28c43b5901509c8e2f0
Marjoy M. Caranto <marjoycaranto29@gmail.com>
2025-10-19 14:06:07 +0800

    chore(main, menu): add templates for menu and main program

---
+++ exercise_3/menu.py
@@ -0,0 +1,28 @@
+"""
+Module for linked list menu operations.
+"""
+
+from linked_list import LinkedList
+
+class Menu:
+    def __init__(self):
+        self.linked_list = LinkedList()
+
+    def valid_choice(self, choice):  # Validate user choice
+        return choice in ['1', '2', '3', '4', '5', '6']
+
+    """Check the linked_list.py file for the LinkedList class implementation."""
+    def insert_node(self):  # Insert a node
+        pass
+
+    def delete_node(self):  # Delete a node
+        pass
+
+    def search_node(self):  # Search for a node
+        pass
+
+    def display_list(self):  # Display the linked list
+        pass
+
+    def reverse_list(self):  # Reverse the linked list
+        pass
